52d21340e68db3d10d01763dad981db8051fed08
Mihail Minchev <makitta811@gmail.com>
2023-01-16 10:27:35 +0200

    e de

--- GraphTheory/GraphTheory/Program.cs
+++ GraphTheory/GraphTheory/Program.cs
@@ -124,7 +124,10 @@ namespace GraphTheory
 
         static void Main(string[] args)
         {
-
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
             Console.ReadKey();
         }
     }
