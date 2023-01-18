117fb85908cb80c3bcc0c6140f5988502cee582b
Mihail Minchev <108220518+makitta811@users.noreply.github.com>
2023-01-18 14:13:22 +0200

    eto dve

--- GraphTheory/GraphTheory/Program.cs
+++ GraphTheory/GraphTheory/Program.cs
@@ -124,7 +124,38 @@ namespace GraphTheory
 
         static void Main(string[] args)
         {
-            Console.WriteLine("To day is a 2023 seventeen th january and this is daily commith :)");
+            // No of vertices
+            int v = 8;
+
+            // Adjacency list for storing
+            // which vertices are connected
+            List<List<int>> adj = new List<List<int>>(v);
+
+            for (int i = 0; i < v; i++)
+            {
+                adj.Add(new List<int>());
+            }
+
+            // Creating graph given in the
+            // above diagram. add_edge
+            // function takes adjacency list,
+            // source and destination vertex
+            // as argument and forms an edge
+            // between them.
+            AddEdge(adj, 0, 1);
+            AddEdge(adj, 0, 3);
+            AddEdge(adj, 1, 2);
+            AddEdge(adj, 3, 4);
+            AddEdge(adj, 3, 7);
+            AddEdge(adj, 4, 5);
+            AddEdge(adj, 4, 6);
+            AddEdge(adj, 4, 7);
+            AddEdge(adj, 5, 6);
+            AddEdge(adj, 6, 7);
+            int source = 0, dest = 7;
+
+
+            PrintShortestDistance(adj, source, dest, v);
             Console.ReadKey();
         }
     }
